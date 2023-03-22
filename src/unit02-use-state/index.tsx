7f0beee8ad3ab1d239984e957642949d692a0325
chloelo <chloe.lo@ennowell.com>
2023-03-22 17:31:48 +0800

    feat: 🎸 add unit-useState example

--- src/unit02-use-state/index.tsx
+++ src/unit02-use-state/index.tsx
@@ -1,6 +1,24 @@
+import { useState } from "react"
+
+import { User } from './interfaces'
+
 const App = () => {
+  const [user, setUser] = useState<User | null>(null)
+  const fetchUser = () => setUser({
+    name: 'Andy',
+    age: 24,
+    country: 'taiwan',
+    address: {
+      street: 'Mait st.',
+      number: 88,
+      zip: 32400
+    },
+    admin: false
+  })
+
   return (<>
-    <h1>123</h1>
+    <button onClick={fetchUser}>Fetch user on Click</button>
+    {user && <p>Welcome {user.name}</p>}
   </>)
 }
 export default App
---
+++ src/unit02-use-state/interfaces.ts
@@ -0,0 +1,12 @@
+export interface Address {
+  street: string;
+  number: number;
+  zip: number;
+}
+export interface User {
+  name: string;
+  age: number;
+  country: string;
+  address: Address;
+  admin: boolean;
+}
